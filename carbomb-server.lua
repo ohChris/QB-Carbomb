a6746b76d39e0173ea23a9a106b6f7450509bb97
ohChris <72506947+ohChris@users.noreply.github.com>
2020-10-07 08:35:39 -0500

    Update carbomb-server.lua

--- carbomb-server.lua
+++ carbomb-server.lua
@@ -1,19 +1,16 @@
-ESX = nil
+QBCore = nil
 
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
 
-ESX.RegisterUsableItem('ied', function(source)
-    local xPlayer = ESX.GetPlayerFromId(source)
-    if xPlayer.getInventoryItem('ied').count > 0 then
-        TriggerClientEvent('RNG_CarBomb:CheckIfRequirementsAreMet', source)
-    end
+QBCore.Functions.CreateUseableItem("ied", function(source, item)
+    local Player = QBCore.Functions.GetPlayer(source)
+
+    TriggerClientEvent('RNG_CarBomb:CheckIfRequirementsAreMet', source)
 end)
 
 RegisterServerEvent('RNG_CarBomb:RemoveBombFromInv')
 AddEventHandler('RNG_CarBomb:RemoveBombFromInv', function()
-    local xPlayer = ESX.GetPlayerFromId(source)
+    local Player = QBCore.Functions.GetPlayer(source)
 
-    if xPlayer.getInventoryItem('ied').count > 0 then
-        xPlayer.removeInventoryItem('ied', 1)
-    end
+    Player.Functions.AddItem('ied', 1)
 end)
